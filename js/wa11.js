a4cbcd9f9ac3a0bbbd5731cd39fc6a1ad1e18061
zotu4246 <zotu4246@colorado.edu>
2024-04-04 10:23:45 -0600

    wa11

---
+++ js/wa11.js
@@ -0,0 +1,50 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+const fileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+const alts = {
+  "pic1.jpg": "close up of eye",
+  "pic2.jpg": "rock",
+  "pic3.jpg": "white and purple flowers",
+  "pic4.jpg": "artwork",
+  "pic5.jpg": "butterfly",
+};
+
+/* Looping through images */
+
+for (const i of fileNames) {
+  //constructing the src and alt text
+  const nalt = alts[i];
+  const nsrc = `../images/${i}`;
+
+  //update  image for the thumbnail
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", nsrc);
+  newImage.setAttribute("alt", nalt);
+  thumbBar.appendChild(newImage);
+
+  // change display image when a thumbnail is clicked
+  newImage.addEventListener("click", () => {
+    displayedImage.setAttribute("src", nsrc);
+    displayedImage.setAttribute("alt", nalt);
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", () => {
+  const btnClass = btn.getAttribute("class");
+
+  if (btnClass === "dark") {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0, 0, 0, 0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0, 0, 0, 0)";
+  }
+});
