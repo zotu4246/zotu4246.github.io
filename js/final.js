56574a435dfed881cc8d79c7239655e9be49923e
zotu4246 <zotu4246@colorado.edu>
2024-04-30 10:08:26 -0600

    final

--- js/final.js
+++ js/final.js
@@ -3,5 +3,5 @@ const volumeLevel = document.getElementById("volume-level");
 
 slider.addEventListener("input", function () {
   const reversedValue = 100 - this.value;
-  volumeLevel.textContent = `${reversedValue} `;
+  volumeLevel.textContent = `${reversedValue} Volume `;
 });
